762dca537079eaa235ba13ee05e8ef5fc3acee00
Skyler.Ab <85718273+SklyerX@users.noreply.github.com>
2023-10-23 19:36:34 -0400

    mistake fixed in auth endpoint

--- apps/web/src/utils/constants.tsx
+++ apps/web/src/utils/constants.tsx
@@ -1,6 +1,6 @@
 export const API_ENDPOINT = "https://counter.jakeforlife1234.repl.co/api";
 export const AUTH_ENDPOINT =
-  "https://discord.com/api/oauth2/authorize?client_id=1164192753295573114&redirect_uri=https://discord.com/api/oauth2/authorize?client_id=1164192753295573114&redirect_uri=https%3A%2F%2Fcounter.jakeforlife1234.repl.co%2Fapi%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify%20guilds&response_type=code&scope=guilds%20identify";
+  "https://discord.com/api/oauth2/authorize?client_id=1164192753295573114&redirect_uri=https%3A%2F%2Fcounter.jakeforlife1234.repl.co%2Fapi%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify%20guilds";
 export const GITHUB_URL = "https://github.com/sklyerx/discord-bot-dashboard";
 export const DEVELOPER_URL = "https://skylerx.ir?ref=discord-bot-dashboard";
 
